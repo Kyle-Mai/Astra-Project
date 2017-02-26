508bc493a05156515759cd37540f6e8459289561
VulcanDev <theokjoyce@gmail.com>
2017-02-26 00:20:28 -0500

    [Ignore] GitHub bot test

--- src/battle.java
+++ src/battle.java
@@ -2,6 +2,6 @@
 // Describe: Battle logic class
 public class battle {
     public static void main(String[] args){
-        System.out.print("「できます」");
+        System.out.print("「できます！」");
     }
 }
