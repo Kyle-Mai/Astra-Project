bb2860419a74a4bf50b34a641a6950e27c88d11c
VulcanDev <theokjoyce@gmail.com>
2017-02-28 10:30:42 -0500

    Some sprite testing added

--- src/battle.java
+++ src/battle.java
@@ -1,10 +1,22 @@
-import javax.*;
 import java.awt.*;
+import java.io.IOException;
 import javax.microedition.lcdui.game.*;
 // Name: Theo Joyce
-// Describe: Battle logic class
+// Describe: Battle logic class / Sprite test class
 public class battle {
     public static void main(String[] args){
-        System.out.print("「できます！」");
+        System.out.println("「めんどくさい！」"); // Changes to how I feel about the class at the time of editing
+        Image sprite;
+        javax.microedition.lcdui.Image img;
+
+        try {
+            img = javax.microedition.lcdui.Image.createImage("spritesheet.jpg");
+        }catch (IOException e){
+            e.printStackTrace();
+            sprite = null;
+            img = null;
+        }
+        // Sprite deemed unusable
+        Sprite test = new Sprite(img, 90, 90);
     }
 }
